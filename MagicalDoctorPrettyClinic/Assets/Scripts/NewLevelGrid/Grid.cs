587b0480831da3ca796d3bb6ea02b4f2361c7b04
seppvanderwal <34765@ma-web.nl>
2026-03-24 13:18:57 +0100

    Grid UI Visualizer + clean up

--- MagicalDoctorPrettyClinic/Assets/Scripts/NewLevelGrid/Grid.cs
+++ MagicalDoctorPrettyClinic/Assets/Scripts/NewLevelGrid/Grid.cs
@@ -2,22 +2,41 @@ using UnityEngine;
 
 public class Grid
 {
-    private int width;
-    private int height;
+    public int width;
+    public int height;
+    public float cellSize;
     private int[,] gridArray;
 
-    public Grid(int width, int height)
+    public Grid(int width, int height, float cellSize)
     {
         this.width = width;
         this.height = height;
+        this.cellSize = cellSize;
         gridArray = new int[width, height];
-        
-        for (int x = 0; x < gridArray.GetLength(0); x++)
-        {
-            for (int y = 0; y < gridArray.GetLength(1); y++)
-            {
-               
-            }
-        }
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (IsValid(x, y))
+            gridArray[x, y] = value;
+    }
+
+    public int GetValue(int x, int y)
+    {
+        return IsValid(x, y) ? gridArray[x, y] : -1;
+    }
+
+    // Convert a canvas anchoredPosition back to grid coords
+    public Vector2Int GetGridPosition(Vector2 anchoredPos)
+    {
+        return new Vector2Int(
+            Mathf.FloorToInt(anchoredPos.x / cellSize),
+            Mathf.FloorToInt(anchoredPos.y / cellSize)
+        );
+    }
+
+    private bool IsValid(int x, int y)
+    {
+        return x >= 0 && y >= 0 && x < width && y < height;
     }
 }
--- MagicalDoctorPrettyClinic/Assets/Scripts/NewLevelGrid/GridTest.cs
+++ MagicalDoctorPrettyClinic/Assets/Scripts/NewLevelGrid/GridTest.cs
@@ -1,11 +1,52 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridTest : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int width = 10;
+    public int height = 10;
+    public float cellSize = 50f;
+
+    public Canvas canvas;
+    public RectTransform parentContainer;
+    public GameObject cellPrefab;
+
+    public bool centerGrid = true;       // auto-center in parent
+    public Vector2 manualOffset;         // extra nudge on top of centering
+
+    private Grid grid;
+
     void Start()
     {
-        Grid grid = new Grid(10, 10);
+        grid = new Grid(width, height, cellSize);
+        GenerateUIGrid();
     }
-    
+
+    private void GenerateUIGrid()
+    {
+        Vector2 gridSize = new Vector2(width * cellSize, height * cellSize);
+        
+        Vector2 centerOffset = centerGrid ? new Vector2(-gridSize.x / 2f, -gridSize.y / 2f) : Vector2.zero;
+
+        Vector2 totalOffset = centerOffset + manualOffset;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject cellObj = Instantiate(cellPrefab, parentContainer);
+
+                RectTransform rect = cellObj.GetComponent<RectTransform>();
+                if (rect == null)
+                    rect = cellObj.AddComponent<RectTransform>();
+
+                rect.anchorMin = new Vector2(0.5f, 0.5f);
+                rect.anchorMax = new Vector2(0.5f, 0.5f);
+                rect.pivot = Vector2.zero;
+
+                rect.anchoredPosition = new Vector2(x * cellSize, y * cellSize) + totalOffset;
+                rect.sizeDelta = new Vector2(cellSize, cellSize);
+            }
+        }
+    }
 }
