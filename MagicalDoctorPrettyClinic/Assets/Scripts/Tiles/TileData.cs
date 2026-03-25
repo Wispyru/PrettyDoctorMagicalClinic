ca7cc7b12b357261287eb60a15d71f97088d02c5
keesbrown <114744966+keesbrown@users.noreply.github.com>
2026-03-25 09:46:42 +0100

    deleted old scripts

--- MagicalDoctorPrettyClinic/Assets/Scripts/Tiles/TileData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TileData", menuName = "Match3/TileData")]
-public class TileData : ScriptableObject
-{
-    public TileType Type;
-    public Sprite Icon;
-    // Illness weakness/strength data can be added here later
-}
--- MagicalDoctorPrettyClinic/Assets/Scripts/Tiles/TileType.cs
+++
@@ -1,8 +0,0 @@
-public enum TileType
-{
-    TypeA,
-    TypeB,
-    TypeC,
-    TypeD,
-    TypeE
-}
