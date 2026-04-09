d8254f68c0c91e10419c43f058d758949ce69882
keesbrown <114744966+keesbrown@users.noreply.github.com>
2026-04-09 14:28:21 +0200

    added the VolumeSettings Script

---
+++ Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider musicSlider;        
+}
